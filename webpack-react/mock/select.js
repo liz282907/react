d75c02a2ff716f42455c90bca7ed98d78b0aa390
liz282907 <422364804@qq.com>
2016-05-20 11:45:09 +0800

    will add datetime

--- webpack-react/mock/select.js
+++ webpack-react/mock/select.js
@@ -2,7 +2,7 @@ var selectData = {
 	"level1":"实时订单",
 	"dimensions":[
 		{"name":"city","value":["呼和浩特","北京","上海","天津","重庆"]},
-		{"name":"date","value":"2016.04.14"},
+		{"name":"date","value":["2016.04.14"]},
 		{"name":"timegap","value":["1分钟","5分钟","10分钟","15分钟","30分钟","1小时"]},
 		{"name":"payType","value":["支付宝","手Q支付"]}
 	]
